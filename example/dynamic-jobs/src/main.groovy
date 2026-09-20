9fca4d602d3e3ff6b8b01bdc71c5a42b96a03be7
rashi-kakran-iopshub <rashi.kakran@iopshub.com>
2026-09-20 12:34:06 +0530

    changes

--- example/dynamic-jobs/src/main.groovy
+++ example/dynamic-jobs/src/main.groovy
@@ -1,9 +1,10 @@
 //println("HHello World !!!")
 
-@Grab('org.yaml:snakeyaml:1.17')
+//@Grab('org.yaml:snakeyaml:1.17')
+//import org.yaml.snakeyaml.Yaml
 
-import org.yaml.snakeyaml.Yaml
 import hudson.model.*
+import utils.JobUtils
 
 // Function to search for all files ending with .yaml or .yml
 ArrayList searchYamlFiles(String dirPath) {
@@ -28,7 +29,6 @@ def cwd = hudson.model.Executor.currentExecutor()
         .getCurrentWorkspace()
         .absolutize()
 
-
 // Search for YAML files
 pipeline_file_list = searchYamlFiles(cwd.toString())
 
@@ -41,10 +41,11 @@ pipeline_file_list = searchYamlFiles(cwd.toString())
 for (current_pipeline in pipeline_file_list) {
     println("Working on: " + current_pipeline)
     //parsing the yaml content
-    parsed_job_config = new Yaml().load((current_pipeline as File).text)
+    //parsed_job_config = new Yaml().load((current_pipeline as File).text)
+    JobUtils job_config = new JobUtils(current_pipeline)
 
-    println("jobname is : "+parsed_job_config.job_name )
-    job(parsed_job_config.job_name) {
+    println("jobname is : "+job_config.job_name )
+    job(job_config.job_name) {
       steps {
         shell('echo Hello World!')
        }
